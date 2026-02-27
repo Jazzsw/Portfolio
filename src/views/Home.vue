39d38910199e8e99134949543e03a6bfd41aba68
Jazz Stocker-Witterick <j.stockerwitterick@gmail.com>
2026-02-27 00:29:17 -0500

    home page work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,16 @@
 import SplineBackground from '../components/Spline.vue';
 </script>
 
+<script>
+  window.addEventListener('scroll', () => {
+    let scroll = window.scrollY / window.innerHeight;
+    const arrow = document.querySelector('.arrow');
+
+    arrow.style.opacity = 1 - scroll*3;
+  });
+
+</script>
+
 <template>
   <div id="app">
     <section class="hero">
@@ -13,6 +23,22 @@ import SplineBackground from '../components/Spline.vue';
           <p>Welcome to my portfolio! I'm a software developer and UI/UX designer currently pursuing a degree in Software Engineering and working as a UX design intern at RWDI's Orbital Stack</p>
         </div>
       </div>
+
+      <img src="/src/assets/arrow.svg" alt="Scroll Down" class="arrow">
+    </section>
+
+    <section class="projects">
+      <div class="projects_title">WORK / PROJECTS</div>
+
+      <div class="project_items">
+        <div class="project_item">
+          <h3 class="project_title">RWDI ORBITAL STACK</h3>
+          <p>some body text...</p>
+        </div>
+      </div>
+      
+    
+      
     </section>
 
   </div>
@@ -31,8 +57,8 @@ import SplineBackground from '../components/Spline.vue';
 
 .hero_content {
   position: relative;
-  pointer-events: none;
   height: auto;
+  z-index: 2;
 }
 
 .name {
@@ -56,7 +82,7 @@ import SplineBackground from '../components/Spline.vue';
 }
 
 .hero{
-  position: absolute;
+  position: relative;
   top: 0;
   left: 0;
   display: flex;
@@ -67,7 +93,7 @@ import SplineBackground from '../components/Spline.vue';
   text-align: center;
   background: transparent;
   pointer-events: none; 
-  z-index: 2;
+  z-index: 1;
 }
 
 .hero_content, 
@@ -75,6 +101,79 @@ import SplineBackground from '../components/Spline.vue';
 .navbar img,
 .hero a {
   pointer-events: auto !important;
+}
+
+.projects {
+  position: relative;
+  z-index: 0; 
+  background-color: #020005;
+  min-height: 100vh;
+  color: aliceblue;
+}
+
+.projects_title{
+  width: fit-content;
+  text-align: center;
+  margin: 0 auto;
+  color: transparent;
+  background: #3B0081;
+  background: linear-gradient(20deg, rgba(59, 0, 129, 1) 0%, rgba(196, 94, 221, 1) 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  font-family: "Montserrat", sans-serif;
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  font-weight: 400;
+} 
+
+.project_items{
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.project_item{
+  color:rgb(236, 236, 236);
+  font-family: "montserrat", sans-serif;
+  border: 0.5px solid transparent;
+  background: linear-gradient(rgb(10, 10, 10), rgb(10, 10, 10)) padding-box,
+    linear-gradient(20deg, rgba(255, 255, 255, 0.3) 0%, rgba(255, 255, 255, 0.3)), border-box;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  border-radius: 8px;
+}
+.project_item:hover{
+  background: linear-gradient(rgb(10, 10, 10), rgb(10, 10, 10)) padding-box,
+    linear-gradient(20deg, rgba(59, 0, 129, 1) 0%, rgba(196, 94, 221, 1) 100%), border-box;
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+}
+
+.project_title{
+  font-family: "Montserrat", sans-serif;
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin-bottom: 0.5rem;
+}
+
+.arrow {
+  position: absolute;
+  bottom: 2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 3rem;
+  height: auto;
+  animation: bounce 2s infinite;
+}
+@keyframes bounce {
+  0%, 100% {
+    transform: translateX(-50%) translateY(0);
+  }
+  50% {
+    transform: translateX(-50%) translateY(-10px);
+  }
 }
 
 
